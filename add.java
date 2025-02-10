f23b6224f9f2ec0e23648e8b2a0299d11020a3de
SankarYesvinth <sankaryesvinth6@gmail.com>
2025-02-10 17:05:07 +0530

    Updated a = 100

--- add.java
+++ add.java
@@ -2,7 +2,7 @@ class add
 {
     public static void main(String[] args)
     {
-        int a = 10;
+        int a = 100;
         int b = 20;
         int c = a + b;
         System.out.println(c);
